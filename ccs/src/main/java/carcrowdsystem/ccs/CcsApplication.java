4232c8b298a4bf11529f362e005f1c982af61d46
Diogo <diogo.gomes@sptech.school>
2023-03-08 17:10:27 -0300

    Iniciando a Api

---
+++ ccs/src/main/java/carcrowdsystem/ccs/CcsApplication.java
@@ -0,0 +1,13 @@
+package carcrowdsystem.ccs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcsApplication.class, args);
+	}
+
+}
---
+++ ccs/src/test/java/carcrowdsystem/ccs/CcsApplicationTests.java
@@ -0,0 +1,13 @@
+package carcrowdsystem.ccs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
