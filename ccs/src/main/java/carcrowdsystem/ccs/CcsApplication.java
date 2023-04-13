a10e1fdaf08b77d0336b1754fb6bd8b7e1cee0f5
jessicabarreirosm <jessica.martins@sptech.school>
2023-04-13 18:05:56 -0300

    Adicionando novas dependencias e adicionando dados fixos

--- ccs/src/main/java/carcrowdsystem/ccs/CcsApplication.java
+++ ccs/src/main/java/carcrowdsystem/ccs/CcsApplication.java
@@ -3,14 +3,16 @@ package carcrowdsystem.ccs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*{
+/*
+{
     "nome": "nome",
     "rg": "rg",
     "cpf": "111.222.333-44",
     "email": "email@gmail.com",
     "telefone": "telefonea",
     "senha": "senha"
-}*/
+}
+*/
 
 @SpringBootApplication
 public class CcsApplication {
---
+++ ccs/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into funcionario_entity (id, cargo, cpf, email, logado, nome, rg, senha, telefone) values
+(1,'gerente','111.222.333-44','email@1email.com',false,'Nome1','15.151.187-1','senha','11999222221'),
+(2,'funcionario','112.222.333-44','email2@email.com',false,'Nome2','15.151.187-2','senha','11999222222'),
+(3,'funcionario','113.222.333-44','email3@email.com',false,'Nome3','15.151.187-3','senha','11999222223'),
+(4,'funcionario','114.222.333-44','email4@email.com',false,'Nome4','15.151.187-4','senha','11999222224'),
+(5,'funcionario','115.222.333-44','email5@email.com',false,'Nome5','15.151.187-5','senha','11999222225'),
+(6,'funcionario','116.222.333-44','email6@email.com',false,'Nome6','15.151.187-6','senha','11999222226');
