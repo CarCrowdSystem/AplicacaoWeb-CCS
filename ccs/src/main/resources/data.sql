8f8cc246e7575474da4abc0ab0ae512203ab34de
Diogo <diogo.gomes@sptech.school>
2023-05-08 19:40:51 -0300

    data.sql, correto

---
+++ ccs/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO ESTACIONAMENTO_ENTITY
+(id, cep, nome_estacionamento, numero_endereco, status_estacionamento, telefone) VALUES
+(1, '00000-001', 'estacionamento1', '001', 0, '99999-9991'),
+(2, '00000-002', 'estacionamento2', '002', 1, '99999-9992'),
+(3, '00000-003', 'estacionamento3', '003', 1, '99999-9993'),
+(4, '00000-004', 'estacionamento4', '004', 0, '99999-9994');
