d134b984f6f44baa4235ef19a9c61c48ffe768a6
Diogo <diogo.gomes@sptech.school>
2023-11-07 19:40:16 -0300

    corrigindo

--- ccs/src/main/java/carcrowdsystem/ccs/CcsApplication.java
+++ ccs/src/main/java/carcrowdsystem/ccs/CcsApplication.java
@@ -5,26 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 
-/*
-Post func
-{
-    "nome": "nome",
-    "rg": "rg",
-    "cpf": "111.222.333-44",
-    "email": "email@gmail.com",
-    "telefone": "telefonea",
-    "senha": "senha"
-}
-
-Post estacionamento
-{
-	"nomeEstacionamento": "estacionamento",
-	"cep": "00000-000",
-	"numeroEndereco": "9",
-	"telefone": "2222-2222"
-}
-*/
-
 @SpringBootApplication
 public class CcsApplication {
 	public static void main(String[] args) {
